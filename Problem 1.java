0519bf74e61c5d4d57efdd5e29db16349e9bb9b5
Pratyush189 <99859984+Pratyush189@users.noreply.github.com>
2022-02-25 22:20:37 +0530

    Add files via upload

---
+++ Problem 1.java
@@ -0,0 +1,92 @@
+//Pratyush kumar
+//21CE10047
+
+import java.util.Scanner;
+class student
+{
+    String name;
+    int roll;
+    double marks;
+    int attendance;
+    student(String name,int roll,int attendance)
+    {
+        this.name = name;
+        this.roll = roll;
+        this.attendance = attendance;
+    }
+    String getName()
+    {
+        return name;
+    }
+    void setName(String name)
+    {
+        this.name = name;
+    }
+    int getRoll()
+    {
+        return roll;
+    }
+    void setRoll(int roll)
+    {
+        this.roll = roll;
+    }
+    double getMarks()
+    {
+        return marks;
+    }
+    void setMarks(double marks)
+    {
+        if(marks<0)
+        {
+            this.marks = 0;
+        }
+        else if(marks > 100)
+        {
+            this.marks = 100;
+        }
+        else 
+        {
+            this.marks = marks;
+        }
+    }
+    int getAttendance()
+    {
+        return attendance;
+        
+    }
+    void setAttendance(int attendance)
+    {
+        this.attendance = attendance;
+    }
+}
+public class Main
+{
+    public static void main(String args[])
+    {
+        String name;
+        int roll;
+        double marks;
+        int attendance;
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("------------------------------------------Enter details of student------------------------------------");
+        System.out.println("Enter Name:");
+        name = Scanner.nextLine();
+        System.out.println("Enter Roll Number:");
+        roll = Scanner.nextInt();
+        System.out.println("Enter Marks:");
+        marks = Scanner.nextDouble();
+        System.out.println("Enter Attendance:");
+        attendance = Scanner.nextInt();
+        student s1 = new student(name,roll,attendance);
+        s1.setMarks(marks);
+        System.out.println("Name: "+s1.name+"  "+" Marks: "+s1.marks+"  "+" Roll No.: "+s1.roll+"  "+" Attendance: "+s1.attendance);
+        if(s1.attendance>75)
+        {
+            s1.setMarks(s1.marks+10);
+        }
+        System.out.println("After change: Name: "+s1.name+"  "+" Marks: "+s1.marks+"  "+" Roll No.: "+s1.roll+"  "+" Attendance: "+s1.attendance);
+        System.out.println("--------------------------------------------------------------------------------------------------------");
+
+    }
+
+}
---
+++ Problem 2.java
@@ -0,0 +1,68 @@
+//Pratyush kumar
+//21CE10047
+
+import java.util.Scanner;
+class Car
+{
+    int speed;
+    int regularPrice;
+    String color;
+    Car(int speed,int regularPrice,String color)
+    {
+        this.speed = speed;
+        this.regularPrice = regularPrice;
+        this.color = color;
+        
+    }
+    int getSalePrice()
+    {
+        return regularPrice;
+    }
+
+}
+class Truck extends Car
+{
+    int weight;
+    Truck(int speed,int regularPrice,String color,int weight)
+    {
+        super(speed,regularPrice,color);
+        this.weight = weight;
+    }
+    int getSalePrice()
+    {
+        if(weight>2300)
+        {
+            System.out.println("Discount on sale Price(10%): "+regularPrice/100);
+            return regularPrice*9/100;
+        }
+        else
+        {
+            System.out.println("Discount on sale Price(20%): "+2*regularPrice/100);
+            return regularPrice*8/100;
+        }
+    }
+    
+}
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("------------------------------Enter details of truck: ------------------------------------------");
+        int speed;
+        int regularPrice;
+        String color;
+        int weight;
+        System.out.println("Enter Colour: ");
+        color = sc.nextLine();
+        System.out.println("Enter speed: ");
+        speed = sc.nextInt();
+        System.out.println("Enter Price: ");
+        regularPrice = sc.nextInt();
+        System.out.println("Enter Weight: ");
+        weight = sc.nextInt();
+        Car t1 = new Truck(speed,regularPrice,color,weight);
+        System.out.println("Speed: "+t1.speed+" Price: "+t1.regularPrice+" Colour: "+t1.color+" Sale Price: "+t1.getSalePrice());
+        System.out.println("-------------------------------------------------------------------------------------------------");
+    }
+}
